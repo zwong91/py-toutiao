b1933f07c5000ebabd70cce050fc7cd59083f920
zwong91 <yzbingchuan@gmail.com>
2022-04-20 17:43:04 +0800

    index:  所有视图列表

--- toutiao-backend/toutiao/main.py
+++ toutiao-backend/toutiao/main.py
@@ -17,8 +17,7 @@ app = create_app(DefaultConfig, enable_config_file=True)
 @app.route('/')
 def route_map():
     """
-    主视图，返回所有视图网址
+    主视图，返回所有视图url
     """
-    a = 1 / 0
     rules_iterator = app.url_map.iter_rules()
     return jsonify({rule.endpoint: rule.rule for rule in rules_iterator if rule.endpoint not in ('route_map', 'static')})
