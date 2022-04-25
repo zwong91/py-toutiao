a54ff9ba1c8f623b1354d89b950fc2d0e8d6b441
zwong91 <yzbingchuan@gmail.com>
2022-04-25 17:49:36 +0800

    toutiao:   gunicorn  start  ~/.bash_profile

--- toutiao-backend/scripts/toutiao_app.sh
+++ toutiao-backend/scripts/toutiao_app.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-source ~/.bashrc
+source ~/.bash_profile
 export FLASK_ENV=production
-cd /home/wz/hm-toutiao/toutiao-backend/
+cd $HOME/py-toutiao/toutiao-backend/
 workon toutiao
-exec gunicorn -b 0.0.0.0:8000 --access-logfile /home/wz/logs/access_app.log --error-logfile /home/wz/logs/error_app.log toutiao.main:app
+exec gunicorn -w 4 -b 0.0.0.0:8000 --access-logfile $HOME/py-toutiao/toutiao-backend/toutiao/logs/access_app.log --error-logfile $HOME/py-toutiao/toutiao-backend/toutiao/logs/error_app.log toutiao.main:app
